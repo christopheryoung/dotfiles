1e6c758ea2550cc76a3fc3d34a926a4d080f778f
Christopher Young <chris@chrisyoung.net>
2012-05-10 16:55:50 -0400

    Added eastwood (clojure linter)

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,7 @@
 {:user {:plugins [[lein-marginalia "0.7.0"]
                   [lein-pprint "1.1.1"]
                   [lein-swank "1.4.3"]
-                  [lein-midje "2.0.0-SNAPSHOT"]]}}
+                  [lein-midje "2.0.0-SNAPSHOT"]
+                  [jonase/eastwood "0.0.2"]]}}
+
+{:eastwood {:exclude-linters [:misplaced-docstring]}}
