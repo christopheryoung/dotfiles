ceacf6bdc93b8e145519e252707a4aa6427ae742
Christopher Young <chris@chrisyoung.net>
2012-05-11 08:55:56 -0400

    Eastwood linter set up properly in profiles.clj now

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,9 @@
 
-{:user {:plugins [[lein-marginalia "0.7.0"]
-                  [lein-pprint "1.1.1"]
-                  [lein-swank "1.4.3"]
-                  [lein-midje "2.0.0-SNAPSHOT"]
-                  [jonase/eastwood "0.0.2"]]}}
+{:user
+ {:plugins [[lein-marginalia "0.7.0"]
+            [lein-pprint "1.1.1"]
+            [lein-swank "1.4.3"]
+            [lein-midje "2.0.0-SNAPSHOT"]
+            [jonase/eastwood "0.0.2"]]
+  :eastwood {:exclude-linters [:misplaced-docstrings]}}}
 
-{:eastwood {:exclude-linters [:misplaced-docstrings]}}
