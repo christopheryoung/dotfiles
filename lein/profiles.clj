799fca4e70fb2eefcf717d27e007d2b68d50b058
Christopher Young <chris@chrisyoung.net>
2012-05-11 08:30:24 -0400

    Fixed typo in excluded linter name

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -5,4 +5,4 @@
                   [lein-midje "2.0.0-SNAPSHOT"]
                   [jonase/eastwood "0.0.2"]]}}
 
-{:eastwood {:exclude-linters [:misplaced-docstring]}}
+{:eastwood {:exclude-linters [:misplaced-docstrings]}}
