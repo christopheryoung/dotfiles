210ceb2d2cd4c6c2776bb5c934a7a6f8175337fc
Christopher Young <chris@chrisyoung.net>
2012-12-11 15:01:47 -0500

    Make sure ace jump mode continues to work in python-mode

--- emacs.d/custom/custom-init-python.el
+++ emacs.d/custom/custom-init-python.el
@@ -3,4 +3,9 @@
 (setq py-shell-name "ipython")
 (require 'ac-python)
 
+(add-hook 'python-mode-hook
+          (progn
+            (local-set-key (kbd "C-o") 'ace-jump-mode)))
+
 (provide 'custom-init-python)
+
