246a7249a4495139ba04b7b68c44e2955d1c60f4
Christopher Young <chris@chrisyoung.net>
2012-12-19 10:20:17 -0500

    Add elisp slime nav mode

--- emacs.d/custom/custom-init-elisp.el
+++ emacs.d/custom/custom-init-elisp.el
@@ -1,4 +1,7 @@
 
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode t)
 
+(autoload 'elisp-slime-nav-mode "elisp-slime-nav")
+(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
+
 (provide 'custom-init-elisp)
