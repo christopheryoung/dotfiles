d671a6d821d37b25a59bf3962b90bdb4510dccce
Christopher Young <chris@chrisyoung.net>
2013-07-30 20:31:02 -0400

    Add duplicate current line or region

--- emacs.d/custom/custom-defuns.el
+++ emacs.d/custom/custom-defuns.el
@@ -9,6 +9,27 @@
   (let ((fill-column (point-max)))
     (fill-paragraph nil)))
 
+;; Thanks: http://tuxicity.se/emacs/elisp/2010/03/11/duplicate-current-line-or-region-in-emacs.html
+(defun duplicate-current-line-or-region (arg)
+  "Duplicates the current line or region ARG times.
+If there's no region, the current line will be duplicated. However, if
+there's a region, all lines that region covers will be duplicated."
+  (interactive "p")
+  (let (beg end (origin (point)))
+    (if (and mark-active (> (point) (mark)))
+        (exchange-point-and-mark))
+    (setq beg (line-beginning-position))
+    (if mark-active
+        (exchange-point-and-mark))
+    (setq end (line-end-position))
+    (let ((region (buffer-substring-no-properties beg end)))
+      (dotimes (i arg)
+        (goto-char end)
+        (newline)
+        (insert region)
+        (setq end (point)))
+      (goto-char (+ origin (* (length region) arg) arg)))))
+
 ;; jump to matching paren
 ;; Thanks to https://github.com/avar/dotemacs/blob/master/.emacs
 (defun match-paren (arg)
