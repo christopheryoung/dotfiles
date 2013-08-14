d7f17a4d6e3b96a1e3313cb4ae1b26ceacb86811
Christopher Young <chris@chrisyoung.net>
2013-08-14 12:31:37 -0400

    Add ein configuration for python mode

--- emacs.d/custom/custom-init-python.el
+++ emacs.d/custom/custom-init-python.el
@@ -1,11 +1,22 @@
 
 (require 'python-mode)
+
 (setq py-shell-name "ipython")
+
 (require 'ac-python)
 
 (add-hook 'python-mode-hook
           (progn
             (local-set-key (kbd "C-o") 'ace-jump-mode)))
+
+;; EIN
+
+(require 'ein)
+(require 'ein-ac)
+(setq ein:use-auto-complete t)
+(setq ein:use-smartrep t)
+
+(require 'pydoc-info)
 
 (provide 'custom-init-python)
 
