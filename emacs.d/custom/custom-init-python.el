fe56c0735fbe4f7bf630bce57a2893be6ad20ff2
Christopher Young <chris@chrisyoung.net>
2013-10-14 09:07:27 -0400

    Fixes to get elpy (sort of) working

--- emacs.d/custom/custom-init-python.el
+++ emacs.d/custom/custom-init-python.el
@@ -1,12 +1,4 @@
 
-(require 'python-mode)
-
-(autoload 'python-mode "python-mode" "Python Mode." t)
-(add-to-list 'auto-mode-alist '("\\.py\\'" . elpy-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-
-(require 'ipython)
-
 (require 'python-pylint)
 (autoload 'pylint "pylint")
 (add-hook 'python-mode-hook 'pylint-add-menu-items)
@@ -29,14 +21,22 @@
       python-shell-completion-module-string-code "")
 
 (add-hook 'python-mode-hook
-          (progn
-            (local-set-key (kbd "C-o") 'ace-jump-mode)
+          '(lambda ()
+             (local-set-key (kbd "C-o") 'ace-jump-mode)
+             (elpy-mode)))
 
-            (elpy-mode 1)
-            (elpy-use-ipython)))
-
+(setq elpy-rpc-backend "jedi")
+(package-initialize)
+;; why do I need to do this again? elpy-enable doesn't
+;; seem to cooperate unless I do
 (elpy-enable)
 (elpy-use-ipython)
 (require 'pydoc-info)
+
+;; getting errors unless I add this hook; eldoc is still on even with this
+;; hook, which is good, since it then works, but also puzzling (perhaps with
+;; this hook I'm disabling a different or improperly configured version of
+;; el-doc that interferes with the one that elpy sets up for me?).
+(add-hook 'elpy-mode-hook '(lambda () (eldoc-mode nil))) 
 
 (provide 'custom-init-python)
