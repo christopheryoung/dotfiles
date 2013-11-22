06539e9b5ceec0a22de5c7db1cd6e973bfd6477c
Christopher Young <chris@chrisyoung.net>
2013-11-22 13:29:52 -0500

    Make the modules directory too

--- bin/mmake.sh
+++ bin/mmake.sh
@@ -1,6 +1,8 @@
-#!bin/bash
+#!/bin/bash
 
 # First make install mmshare and then set up the appropriate symlinks
 cd $SCHRODINGER/mmshare-v*/python/scripts
 make install
+cd $SCHRODINGER/mmshare-v*/python/modules
+make schrodinger_modules
 python ~/bin/symlink_mmshare.py
