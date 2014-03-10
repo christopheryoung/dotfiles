8c3915898e6c4da9d9654d542ec724aaed30a7cc
Christopher Young <chris@chrisyoung.net>
2014-03-10 15:17:41 -0400

    Add rough but working Python configuration

--- emacs.d/custom/custom-init-python.el
+++ emacs.d/custom/custom-init-python.el
@@ -1,41 +1,38 @@
 
-(require 'python-pylint)
-(autoload 'pylint "pylint")
-(add-hook 'python-mode-hook 'pylint-add-menu-items)
-(add-hook 'python-mode-hook 'pylint-add-key-bindings)
-
-(setq py-shell-name "ipython")
-
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args ""
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
- "from IPython.core.completerlib import module_completion")
-
-(setq py-python-command-args '("--colors=linux"))
-
-(setq python-shell-completion-string-code
-      "';'.join(__IP.complete('''%s'''))\n"
-      python-shell-completion-module-string-code "")
+(defvar server-buffer-clients)
+(when (and (fboundp 'server-start) (string-equal (getenv "TERM") 'xterm))
+  (server-start)
+  (defun fp-kill-server-with-buffer-routine ()
+    (and server-buffer-clients (server-done)))
+  (add-hook 'kill-buffer-hook 'fp-kill-server-with-buffer-routine))
 
 (add-hook 'python-mode-hook
           '(lambda ()
              (local-set-key (kbd "C-o") 'ace-jump-mode)))
 
-(setq elpy-rpc-backend "jedi")
-(package-initialize)
-;; why do I need to do this again? elpy-enable doesn't
-;; seem to cooperate unless I do
-(elpy-enable)
-(elpy-use-ipython)
-(require 'pydoc-info)
-
 ;; getting errors unless I add this hook; eldoc is still on even with this
 ;; hook, which is good, since it then works, but also puzzling (perhaps with
 ;; this hook I'm disabling a different or improperly configured version of
 ;; el-doc that interferes with the one that elpy sets up for me?).
-(add-hook 'elpy-mode-hook '(lambda () (eldoc-mode nil))) 
+(add-hook 'python-mode-hook '(lambda () (eldoc-mode)))
+(add-hook 'elpy-mode-hook '(lambda () (eldoc-mode))) 
+
+(package-initialize)
+;; why do I need to do this again? elpy-enable doesn't
+;; seem to cooperate unless I do
+(elpy-enable)
+(setq elpy-rpc-backend "jedi")
+
+(elpy-use-ipython)
+;;(require 'pydoc-info)
+
+(defun python-helm-docs ()
+  (interactive)
+  (setq-local helm-dash-docsets '("Python_2")))
+
+(add-hook 'python-mode-hook 'python-helm-docs)
+
+(eval-after-load "helm-dash"
+  '(defun helm-dash-actions (actions doc-item) `(("Go to doc" . eww))))
 
 (provide 'custom-init-python)
