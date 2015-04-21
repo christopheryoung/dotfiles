aa6cb747ddfa227714b8084af87dcea034a7eb14
Christopher Young <chris@chrisyoung.net>
2015-04-21 11:25:44 -0400

    Fiddle with Haskell mode

--- emacs.d/custom/custom-init-haskell.el
+++ emacs.d/custom/custom-init-haskell.el
@@ -1,18 +1,18 @@
 
-
 (require 'auto-complete-haskell)
 (require 'hsenv)
+
+(custom-set-variables '(haskell-process-type 'cabal-repl))
 
 (setenv "PATH" (concat "~/.cabal/bin:" (getenv "PATH")))
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (add-hook 'haskell-mode-hook 'haskell-font-lock-symbols t)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-to-list 'completion-ignored-extensions ".hi")
 
-(custom-set-variables
- '(haskell-mode-hook '(turn-on-haskell-indentation)))
-
+(eval-after-load 'haskell-mode
+          '(define-key haskell-mode-map [f8] 'haskell-navigate-imports))
 (define-key haskell-mode-map "\C-ch" 'haskell-hoogle)
 (setq haskell-hoogle-command "hoogle")
 
@@ -30,5 +30,23 @@
     (add-hook
      (intern (concat (symbol-name x)
                      "-mode-hook")))))
+
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
+(eval-after-load 'haskell-mode '(progn
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+(eval-after-load 'haskell-cabal '(progn
+  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
 
 (provide 'custom-init-haskell)
