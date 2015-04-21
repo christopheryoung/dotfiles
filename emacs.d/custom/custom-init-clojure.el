6c27b6b250e017caf922870121d35b1660a44b94
Christopher Young <chris@chrisyoung.net>
2015-04-21 11:25:22 -0400

    Delete clojure stuff until I return to language

--- emacs.d/custom/custom-init-clojure.el
+++ emacs.d/custom/custom-init-clojure.el
@@ -1,29 +1,4 @@
 
-(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
-
-(add-hook 'clojure-mode-hook
-          (lambda ()
-            (require 'midje-mode)
-            (require 'clojure-jump-to-file)
-            (imenu-add-menubar-index)
-            (local-set-key (kbd "C-c C-j") 'nrepl-jack-in)
-            (local-set-key (kbd "C-c C-,") 'midje-check-fact)
-            (local-set-key (kbd "C-z") 'nrepl-eval-expression-at-point)))
-
-;; Macs are odd; had to do this to get clojure-jack-in working
-(if *on-a-mac*
-    (setenv "PATH" (concat "~/bin:" (getenv "PATH"))))
-
-(setenv "PATH" (shell-command-to-string "echo $PATH"))
-
-(require 'ac-nrepl)
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'clojure-nrepl-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'nrepl-mode))
-
-(global-set-key (kbd "C-h C-j") 'javadoc-lookup)
-
 ;; Functions that make it even easier to interact with clojure in emacs.
 
 (defun symbol-at-point-to-string ()
