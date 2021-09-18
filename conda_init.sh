628b07e9a7769a5bc3b76597ce3ba9b818e430dc
Christopher Young <chris@chrisyoung.net>
2021-09-18 14:33:52 -0400

    Special conda init in case I need it

---
+++ conda_init.sh
@@ -0,0 +1,16 @@
+# added by Anaconda3 2019.07 installer
+# >>> conda init >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$(CONDA_REPORT_ERRORS=false '/Users/young/anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    \eval "$__conda_setup"
+else
+    if [ -f "/Users/young/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/young/anaconda3/etc/profile.d/conda.sh"
+        CONDA_CHANGEPS1=false conda activate base
+    else
+        \export PATH="/Users/young/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda init <<<
