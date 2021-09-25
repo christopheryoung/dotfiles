7b41788d42ab0e6510029af7b9055a21a9e9263c
Christopher Young <chris@chrisyoung.net>
2021-09-25 11:44:56 -0400

    Improve Python customization

--- emacs.d/custom/custom-init-python.el
+++ emacs.d/custom/custom-init-python.el
@@ -1,47 +1,51 @@
+;; Generic Python
 (add-hook 'python-mode-hook
 	  '(lambda ()
 	     (local-set-key (kbd "C-o") 'ace-jump-mode)
-	     (rainbows-delimiters-mode)))
+	     (local-set-key (kbd "M-i") 'helm-swoop)
+	     (local-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+	     (local-set-key (kbd "C-c C-f") 'custom-find-python-func)
+	     (eldoc-mode)
+	     (add-hook 'before-save-hook 'whitespace-cleanup)
+	     ))
 
+(setq python-fill-docstring-style 'django)
+
+(defun custom-find-python-func ()
+  "Finds the Python function definitions in the project"
+  (interactive)
+  (elpy-rgrep-symbol (concat "def " (thing-at-point 'symbol))))
+
+;; Elpy
+
+(eval-after-load "elpy" '(define-key elpy-mode-map (kbd "C-c C-d") 'custom-find-python-func))
+(eval-after-load "python" '(define-key python-mode-map (kbd "C-c C-d") 'custom-find-python-func))
+
+(add-hook 'elpy-mode-hook '(lambda ()
+			     (eldoc-mode)
+			     ;;(flycheck-mode)
+			     (setq python-fill-docstring-style 'django)
+			     ;;(add-hook 'before-save-hook 'whitespace-cleanup)
+			     ))
+
+(setq python-yapf-path "/scr/young/builds/2021-4/build/buildvenv/*/bin/yapf")
+(elpy-enable)
+
+(setq flycheck-python-flake8-executable "/scr/young/builds/2021-4/build/buildvenv/*/bin/flake8")
+;;(flycheck-select-checker 'python-flake8)
+(flycheck-mode t)
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; Jedi
 (add-hook 'python-mode-hook 'jedi:setup)
 
-;; look at C-h v : jedi:server-args
 (defvar jedi-config:vcs-root-sentinel ".git")
 
 ;; getting errors unless I add this hook; eldoc is still on even with this
 ;; hook, which is good, since it then works, but also puzzling (perhaps with
 ;; this hook I'm disabling a different or improperly configured version of
 ;; el-doc that interferes with the one that elpy sets up for me?).
-(add-hook 'python-mode-hook '(lambda () (eldoc-mode)))
-(add-hook 'elpy-mode-hook '(lambda ()
-			     (eldoc-mode)
-			     (whitespace-mode t)
-			     (rainbows-delimiters-mode)
-			     (flycheck-mode)
-			     (local-set-key (kbd "C-h C-j") 'elpy-show-defun)))
 
-(setq python-fill-docstring-style 'django)
-
-(setq python-yapf-path "/Users/young/code/schrodinger/buildvenv/*/bin/yapf")
-
-(let ((workon-home (expand-file-name "~/.virtualenvs")))
-  (setenv "WORKON_HOME" workon-home)
-  (setenv "VIRTUALENVWRAPPER_HOOK_DIR" workon-home))
-
-;; why do I need to do this again? elpy-enable doesn't
-;; seem to cooperate unless I do
-(elpy-enable)
-(setq elpy-rpc-backend "rope")
-
-(elpy-use-ipython)
-
-;;----------
-;; Keybinding to add breakpoint:
-
-(add-hook 'python-mode-hook
-	  (function (lambda ()
-		    (add-hook 'before-save-hook 'whitespace-cleanup))))
-
-(elpy-enable)
 
 (provide 'custom-init-python)
