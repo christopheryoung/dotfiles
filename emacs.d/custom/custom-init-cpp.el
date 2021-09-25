75b762682c9fa965ac9aae4391201d25c1f6d61c
Christopher Young <chris@chrisyoung.net>
2021-09-25 11:44:21 -0400

    Update custom cpp

--- emacs.d/custom/custom-init-cpp.el
+++ emacs.d/custom/custom-init-cpp.el
@@ -1,4 +1,27 @@
+(setq schrodinger-clang-format "/software/lib/Darwin-x86_64/llvm+clang-3.7.1/bin/clang-format")
+(setq clang-format-executable schrodinger-clang-format)
+(setq-default c-basic-offset 4)
 
-(setq clang-format-executable "/software/lib/Darwin-x86_64/llvm+clang-3.7.1/bin/clang-format")
+(defun check-clang-format-exists ()
+  (interactive)
+    (if (not (file-exists-p schrodinger-clang-format))
+	(message "Update your clang-format!")))
 
+(defun clang-format-before-save ()
+  (interactive)
+  (when (eq major-mode 'c++-mode) (clang-format-buffer)))
+
+(add-hook 'c++-mode-hook
+	  '(lambda ()
+	     (add-hook 'clang-format-buffer nil 'make-it-local)
+	     (add-hook 'before-save-hook 'clang-format-before-save)))
+
+(setq lsp-clients-clangd-executable "/usr/local/opt/llvm/bin/clangd")
+(setq cquery-executable "/usr/local/bin/cquery")
 (require 'cquery)
+
+
+(require 'lsp-ui)
+(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+
+(provide 'custom-init-cpp)
