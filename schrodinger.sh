25a585454666549351c90bbda8fd9d186429da39
Christopher Young <chris@chrisyoung.net>
2021-09-25 11:29:42 -0400

    Add a few items to work bash start up

--- schrodinger.sh
+++ schrodinger.sh
@@ -19,9 +19,37 @@ function reportenv() {
     echo $SCHRODINGER_SRC
 }
 
+# Sneak a yapf fix into the latest commit
+function sneak_yapf() {
+    yapf -i $1 && git add $1 && git cram
+}
+
+y () {
+start_dir=$(pwd)
+branch=${1:-master}
+repo_top_dir=$(git rev-parse --show-toplevel)
+cd $repo_top_dir && yapf -i $(git diff --name-only $branch '*.py'); cd $start_dir
+}
+
+# Run mmshare tests, pipe output to a file, and then transform it so it can
+# serve as an effective compilation buffer in the event there are failures
+function mtest() {
+    mmake && make unittest TEST_ARGS="$1" 2>&1 | tee ~/test_output.txt && python3 ~/bin/transform_test_output.py ~/test_output.txt $SCHRODINGER_SRC
+}
+
+
+if [[ $platform == 'mac' ]]; then
+    code_base='/Users/young/builds'
+elif [[ $platform == 'linux' ]]; then
+    code_base='/scr2/young'
+elif [[ $platform == 'win' ]]; then
+    code_base='/home/young/code/builds'
+fi
+
+
 function workon() {
-    export SCHRODINGER=/scr/young/builds/$1/build
-    export SCHRODINGER_SRC=/scr/young/builds/$1/source
+    export SCHRODINGER=$code_base/$1/build
+    export SCHRODINGER_SRC=$code_base/$1/source
     reportenv
 }
 
@@ -34,12 +62,15 @@ fi
 export PATH=$PATH:/utils/bin
 export SCHRODINGER_LIB='/software/lib'
 export PATH=$PATH:/software/lib/common
-export INTEL_LICENSE_FILE=28518@ns4:28518@ns3
+export SCHRODINGER_SHOW_QT_MESSAGES=1
 
 function postr() {
     branch=$(git rev-parse --abbrev-ref HEAD)
     rbt post --bugs-closed=$branch --description "" --summary ""
 }
+
+# getv
+source ~/code/schnippets/bash/functions/getv.sh
 
 alias buildinger='$SCHRODINGER_SRC/mmshare/build_tools/buildinger.sh'
 alias ssch='source $SCHRODINGER_SRC/mmshare/build_env'
@@ -58,7 +89,8 @@ alias mmlog='less $SCHRODINGER/mmshare-v*/make_mmshare_all.log'
 
 if [[ $platform == 'mac' ]]; then
     export MAESTRO_SCRIPTS='/Users/young/code/maestro_scripts'
-    alias designer='$SCHRODINGER_LIB/Darwin-x86_64/qt-5.9.*/bin/Designer.app/Contents/MacOS/Designer'
+    export QTDIR="$SCHRODINGER_LIB/Darwin-x86_64/qt-*"
+    alias designer='open $QTDIR/bin/Designer.app'
     alias run_msv='mmake && run $SCHRODINGER/mmshare-v*/lib/Darwin-x86_64/lib/python2.7/site-packages/schrodinger/application/msv/gui/msv_gui.py'
 
 elif [[ $platform == 'linux' ]]; then
