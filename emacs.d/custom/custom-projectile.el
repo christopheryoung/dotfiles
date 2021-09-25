1a63e53f6251f1083335740dbeee3733618faa77
Christopher Young <chris@chrisyoung.net>
2021-09-25 11:57:08 -0400

    Use flx-ido with projectile

--- emacs.d/custom/custom-projectile.el
+++ emacs.d/custom/custom-projectile.el
@@ -1,8 +1,8 @@
-
 (projectile-global-mode)
 
-(setq projectile-completion-system 'ivy)
+(setq projectile-completion-system 'flx-ido)
 (define-key projectile-mode-map [?\s-f] 'projectile-find-file)
 (define-key projectile-mode-map [?\s-g] 'projectile-grep)
+(setq projectile-sort-order 'recently-active)
 
 (provide 'custom-projectile)
