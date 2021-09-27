4044cdcb8ff8581de838121e3498c168cf7c4bcf
Christopher Young <chris@chrisyoung.net>
2021-09-27 14:11:27 -0400

    Revert "Clean up ido detritus"
    This reverts commit 6bae01b4c54b46e943556c44816391ba09ef3377.

--- emacs.d/custom/custom-appearance.el
+++ emacs.d/custom/custom-appearance.el
@@ -74,4 +74,14 @@
 ;; Let me *see* the marks
 (visible-mark-mode 1)
 
+;; For ido-vertical-mode
+(setq ido-use-faces t)
+(set-face-attribute 'ido-vertical-first-match-face nil
+		    :background "#e5b7c0")
+(set-face-attribute 'ido-vertical-only-match-face nil
+		    :background "#e52b50"
+		    :foreground "white")
+(set-face-attribute 'ido-vertical-match-face nil
+		    :foreground "#b00000")
+
 (provide 'custom-appearance)
