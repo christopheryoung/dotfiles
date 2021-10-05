becfe40dc9a838ec549f6a6bcc867dc122bd995e
Christopher Young <chris@chrisyoung.net>
2021-10-05 19:31:04 -0400

    Remove marmelade

--- emacs.d/custom/custom-packages.el
+++ emacs.d/custom/custom-packages.el
@@ -1,6 +1,5 @@
 (require 'package)
-(add-to-list 'package-archives
-	     '("marmalade" . "https://marmalade-repo.org/packages/") t)
+
 
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
