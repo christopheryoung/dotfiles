dbe4ce0e4e4bbf0b72a743ee3f8ab8fa55229047
Christopher Young <chris@chrisyoung.net>
2022-01-18 15:35:38 -0500

    Fix org-roam config

--- emacs.d/custom/custom-org-roam.el
+++ emacs.d/custom/custom-org-roam.el
@@ -21,4 +21,11 @@
 
 (setq org-roam-graph-viewer "/Applications/Firefox.app/Contents/MacOS/firefox")
 
+(add-to-list 'display-buffer-alist
+	     '("\\*org-roam\\*"
+	       (display-buffer-in-direction)
+	       (direction . right)
+	       (window-width . 0.33)
+	       (window-height . fit-window-to-buffer)))
+
 (provide 'custom-org-roam)
