6dfa8f2d1279699bdb7f99070bd72ddc1ed7ad09
Christopher Young <chris@chrisyoung.net>
2022-01-18 15:44:40 -0500

    Remove projectile-completion-system

--- emacs.d/custom/custom-projectile.el
+++ emacs.d/custom/custom-projectile.el
@@ -1,6 +1,5 @@
 (projectile-global-mode)
 
-(setq projectile-completion-system 'flx-ido)
 (define-key projectile-mode-map [?\s-f] 'projectile-find-file)
 (define-key projectile-mode-map [?\s-g] 'projectile-grep)
 (setq projectile-sort-order 'recently-active)
