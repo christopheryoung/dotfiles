a5877b825a7824e964fe5998796dbfe879a3a19b
Christopher Young <chris@chrisyoung.net>
2023-07-14 07:16:14 -0400

    Clean up whitespace in fundamental mode

--- emacs.d/custom/custom-basic-behaviour.el
+++ emacs.d/custom/custom-basic-behaviour.el
@@ -127,6 +127,11 @@
 ;; Does not seem to work with chrome
 (setq browse-url-new-window-flag t)
 
+(add-hook 'before-save-hook
+	  (lambda ()
+	    (unless (eq major-mode 'fundamental-mode)
+	      (whitespace-cleanup))))
+
 ;; autocomplete
 (ac-config-default)
 
