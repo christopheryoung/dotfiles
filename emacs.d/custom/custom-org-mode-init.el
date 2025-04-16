43cb29ba0abe02080dbb087c375876dc3c965d68
Christopher Young <christopher.young@schrodinger.com>
2025-04-16 14:21:19 -0400

    Remove org-install

--- emacs.d/custom/custom-org-mode-init.el
+++ emacs.d/custom/custom-org-mode-init.el
@@ -1,6 +1,5 @@
 ;; Just a stub at the moment
 
-(require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
